4eb1cc9625d6d209c176b883576c32080bf825b5
Monte15 <paumonterosa@gmail.com>
2026-02-18 22:58:32 +0100

    Neteja total: eliminats fitxers de Quartz i restaurat Next.js

--- quartz/static/cursor-halo.js
+++
@@ -1,8 +0,0 @@
-(() => {
-  const root = document.documentElement;
-
-  window.addEventListener("mousemove", (e) => {
-    root.style.setProperty("--halo-x", e.clientX + "px");
-    root.style.setProperty("--halo-y", e.clientY + "px");
-  }, { passive: true });
-})();
